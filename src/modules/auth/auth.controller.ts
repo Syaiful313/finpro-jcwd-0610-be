df6ddf7d38c3a08d23dd543323c2c484a463849b
diah <diarifiana@gmail.com>
2025-06-01 11:50:18 +0700

    feat: credential registration and send email verification

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,6 +3,8 @@ import { injectable } from "tsyringe";
 import { AuthService } from "./auth.service";
 import { LoginDTO } from "./dto/login.dto";
 import { RegisterDTO } from "./dto/register.dto";
+import { VerificationDTO } from "./dto/verification.dto";
+import { ResendEmailDTO } from "./dto/resendEmail.dto";
 
 @injectable()
 export class AuthController {
@@ -22,6 +24,34 @@ export class AuthController {
     try {
       const body = req.body as RegisterDTO;
       const result = await this.authService.register(body);
+      res.status(200).send(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  verifyEmailAndSetPassword = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const body = req.body as VerificationDTO;
+      const result = await this.authService.verifyEmailAndSetPassword(body);
+      res.status(200).send(result);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  resendEmailVerification = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const body = req.body as ResendEmailDTO;
+      const result = await this.authService.resendEmailVerification(body);
       res.status(200).send(result);
     } catch (error) {
       next(error);
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -7,6 +7,9 @@ import { LoginDTO } from "./dto/login.dto";
 import { RegisterDTO } from "./dto/register.dto";
 import { PasswordService } from "./password.service";
 import { TokenService } from "./token.service";
+import { MailService } from "../mail/mail.service";
+import { VerificationDTO } from "./dto/verification.dto";
+import { ResendEmailDTO } from "./dto/resendEmail.dto";
 
 @injectable()
 export class AuthService {
@@ -14,6 +17,7 @@ export class AuthService {
     private readonly prisma: PrismaService,
     private readonly passwordService: PasswordService,
     private readonly tokenService: TokenService,
+    private readonly mailService: MailService,
   ) {}
 
   login = async (body: LoginDTO) => {
@@ -25,6 +29,10 @@ export class AuthService {
 
     if (!existingUser) {
       throw new ApiError("User not found", 404);
+    }
+
+    if (!existingUser.password) {
+      throw new ApiError("User is not verified", 400);
     }
 
     const isPasswordValid = await this.passwordService.comparePassword(
@@ -73,8 +81,7 @@ export class AuthService {
   };
 
   register = async (body: RegisterDTO) => {
-    const { firstName, lastName, email, password } = body;
-
+    const { firstName, lastName, email, phoneNumber } = body;
     const existingUser = await this.prisma.user.findFirst({
       where: { email },
     });
@@ -83,16 +90,114 @@ export class AuthService {
       throw new ApiError("Email already exist", 400);
     }
 
-    const hashedPassword = await this.passwordService.hashPassword(password);
-
-    return await this.prisma.user.create({
+    const newUser = await this.prisma.user.create({
       data: {
         firstName,
         lastName,
         email,
-        password: hashedPassword,
+        phoneNumber,
+        isVerified: false,
       },
       select: prismaExclude("User", ["password"]),
     });
+
+    const verificationPayload = { userId: newUser.id, email: newUser.email };
+    const emailVerificationToken = this.tokenService.generateToken(
+      verificationPayload,
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1h" },
+    );
+
+    await this.prisma.user.update({
+      where: { id: newUser.id },
+      data: {
+        emailVerificationToken: emailVerificationToken,
+      },
+    });
+
+    const verificationLink = `${process.env.FRONTEND_URL}/register/set-password?token=${emailVerificationToken}`;
+    await this.mailService.sendVerificationEmail(
+      newUser.email,
+      verificationLink,
+    );
+
+    return newUser;
+  };
+
+  verifyEmailAndSetPassword = async (body: VerificationDTO) => {
+    const { token, password } = body;
+    let decoded: any;
+    try {
+      decoded = this.tokenService.verifyToken(
+        token,
+        process.env.JWT_SECRET as string,
+      );
+    } catch (err) {
+      throw new ApiError("Invalid or expired verification token", 400);
+    }
+
+    const userId = decoded.userId;
+    const userEmail = decoded.email;
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId, email: userEmail },
+    });
+
+    if (
+      !user ||
+      user.isVerified ||
+      !user.emailVerificationToken ||
+      user.emailVerificationToken !== token
+    ) {
+      throw new ApiError("Invalid or already verified user/token", 400);
+    }
+
+    const hashedPassword = await this.passwordService.hashPassword(password);
+    const updatedUser = await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        password: hashedPassword,
+        isVerified: true,
+        emailVerificationToken: null,
+      },
+      select: prismaExclude("User", ["password"]),
+    });
+
+    return updatedUser;
+  };
+
+  resendEmailVerification = async (body: ResendEmailDTO) => {
+    const { email } = body;
+    const existingUser = await this.prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!existingUser) {
+      throw new ApiError("Email is not registered", 400);
+    }
+
+    const verificationPayload = {
+      userId: existingUser.id,
+      email: existingUser.email,
+    };
+    const emailVerificationToken = this.tokenService.generateToken(
+      verificationPayload,
+      process.env.JWT_SECRET as string,
+      { expiresIn: "1h" },
+    );
+
+    await this.prisma.user.update({
+      where: { id: existingUser.id },
+      data: {
+        emailVerificationToken: emailVerificationToken,
+      },
+    });
+
+    const verificationLink = `${process.env.FRONTEND_URL}/register/set-password?token=${emailVerificationToken}`;
+    await this.mailService.sendVerificationEmail(
+      existingUser.email,
+      verificationLink,
+    );
+
+    return existingUser;
   };
 }
